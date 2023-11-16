dcb2efb4300229a6a8a65a865b282d09bfe02299
ivann132 <ivanprayogaadi@gmail.com>
2023-11-16 09:16:41 +0700

    first commit

---
+++ Controller/Controller.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace Controller;
+
+class Controller
+{
+    // VARIABLE ATRIBUT
+    var $controllerName;
+    var $controllerMethod;
+
+    // METHOD UNTUK MENGAMBIL SEMUA ATRIBUT
+    public function getControllerAttribut()
+    {
+        return [
+            "ControllerName" => $this -> controllerName,
+            "Method" => $this -> controllerMethod
+        ];
+    }
+}
